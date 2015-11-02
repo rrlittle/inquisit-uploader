154d6937c2cee5aff9e69d165f3a947b9c07239f
unknown <RRLittle2@Wisc.Edu>
2015-11-02 15:10:04 -0600

    added runningbat file

---
+++ run_upload_inquisit_datfiles.bat
@@ -0,0 +1,11 @@
+cd >> C:\Users\RRLITTLE2\Desktop\test.txt
+
+REM Upload Affective go no-go
+REM python -t data_rdmr_agn_t -f AffectiveGoNogo_datafileAffectiveGoNogo_datafile_*.iqdat -p "S:\data\RDoC Computer Tasks\Inqusit Data\Affective"
+
+REM Upload Hungry Donkey
+REM python -t data_rdmr_hdt_t -f HDT_summary_*.iqdat -p "S:\data\RDoC Computer Tasks\Inqusit Data\HungryDonkeyTask"
+
+REM Upload psap
+REM python -t data_rdmr_psap_t -f PSAP_rawdata_*.iqdat -p "S:\data\RDoC Computer Tasks\Inqusit Data\HungryDonkeyTask"
+
